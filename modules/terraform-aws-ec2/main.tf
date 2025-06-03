cbe4750e0cfa9c21914174fcd6ac47673822c43b
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-03 14:56:07 +0530

    Update main.tf

--- modules/terraform-aws-ec2/main.tf
+++ modules/terraform-aws-ec2/main.tf
@@ -156,7 +156,7 @@ resource "aws_lb_listener_rule" "register" {
 
   action {
     type             = "forward"
-    target_group_arn = target_group_arn = aws_lb_target_group.tg[2].arn
+    target_group_arn = aws_lb_target_group.tg[2].arn
   }
 
   condition {
