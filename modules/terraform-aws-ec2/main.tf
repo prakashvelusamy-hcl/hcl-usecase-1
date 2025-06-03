9dc416e2580e4b6698ec02c1afa03f30ae6e2d23
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-03 13:26:12 +0530

    Update main.tf

--- modules/terraform-aws-ec2/main.tf
+++ modules/terraform-aws-ec2/main.tf
@@ -64,62 +64,62 @@ resource "aws_instance" "public_instances" {
 
 
 
-# resource "aws_security_group" "alb_sg" {
-#   name        = "alb_sg"
-#   description = "Allow HTTP inbound to ALB"
-#   vpc_id      = aws_vpc.main.id
+ resource "aws_security_group" "alb_sg" {
+   name        = "alb_sg"
+   description = "Allow HTTP inbound to ALB"
+   vpc_id      = var.vpc_id
 
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+   ingress {
+     from_port   = 80
+     to_port     = 80
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
-
-# resource "aws_lb" "alb" {
-#   name               = "example-alb"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.alb_sg.id]
-#   subnets            = aws_subnet.public.*.id
-# }
+   egress {
+     from_port   = 0
+     to_port     = 0
+     protocol    = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+ }
 
 
-# resource "aws_lb_target_group" "tg" {
-#   count    = 3
-#   name     = "tg-${count.index}"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = aws_vpc.main.id
-#   target_type = "instance"
-
-#   health_check {
-#     path                = "/"
-#     protocol            = "HTTP"
-#     matcher             = "200"
-#     interval            = 30
-#     timeout             = 5
-#     healthy_threshold   = 2
-#     unhealthy_threshold = 2
-#   }
-# }
+resource "aws_lb" "alb" {
+  name               = "usecase-1"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = var.public_subnet_ids
+ }
 
 
-# resource "aws_lb_target_group_attachment" "attach" {
-#   count            = 3
-#   target_group_arn = aws_lb_target_group.tg[count.index].arn
-#   target_id        = aws_instance.app[count.index].id
-#   port             = 80
-# }
+resource "aws_lb_target_group" "tg" {
+   count    = 3
+   name     = "tg-${count.index}"
+   port     = 80
+   protocol = "HTTP"
+   vpc_id   = var.vpc_id
+   target_type = "instance"
+
+   health_check {
+     path                = "/"
+     protocol            = "HTTP"
+     matcher             = "200"
+     interval            = 30
+     timeout             = 5
+     healthy_threshold   = 2
+     unhealthy_threshold = 2
+   }
+ }
+
+
+ resource "aws_lb_target_group_attachment" "attach" {
+   count            = 3
+   target_group_arn = aws_lb_target_group.tg[count.index].arn
+   target_id        = aws_instance.public_instances[count.index].id
+   port             = 80
+ }
 
 
 # resource "aws_lb_listener" "http" {
@@ -152,4 +152,4 @@ resource "aws_instance" "public_instances" {
 #       values = ["/app${count.index + 1}"]
 #     }
 #   }
-# }
+# }}
