ba0a78f2b5e251447c67511d42acfd6ab7178e4a
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-03 14:57:37 +0530

    Update main.tf

--- modules/terraform-aws-ec2/main.tf
+++ modules/terraform-aws-ec2/main.tf
@@ -123,7 +123,7 @@ resource "aws_lb_target_group" "tg" {
 
 
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.this.arn
+  load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
   default_action {
