9d374e5a0180fb2849cd7cc796a3805cd7cae1e5
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-03 14:38:40 +0530

    Update main.tf

--- modules/terraform-aws-ec2/main.tf
+++ modules/terraform-aws-ec2/main.tf
@@ -122,19 +122,19 @@ resource "aws_lb_target_group" "tg" {
  }
 
 
-# resource "aws_lb_listener" "http" {
-#   load_balancer_arn = aws_lb.alb.arn
-#   port              = 80
-#   protocol          = "HTTP"
-#   default_action {
-#     type = "fixed-response"
-#     fixed_response {
-#       content_type = "text/plain"
-#       message_body = "404: Not Found"
-#       status_code  = "404"
-#     }
-#   }
-# }
+ resource "aws_lb_listener" "http" {
+   load_balancer_arn = aws_lb.alb.arn
+   port              = 80
+   protocol          = "HTTP"
+   default_action {
+    type = "fixed-response"
+     fixed_response {
+       content_type = "text/plain"
+       message_body = "404: Not Found"
+       status_code  = "404"
+     }
+   }
+ }
 
 # resource "aws_lb_listener_rule" "path_rules" {
 #   count = 3
